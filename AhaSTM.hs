f1431466364b295d2becd89f1283cc22ca8569dd
mitsuji <mitsuji@na-s.jp>
2015-09-05 20:07:05 +0900

    ok

--- AhaSTM.hs
+++ AhaSTM.hs
@@ -581,3 +581,136 @@ reporterTalk conn board@Board{..} Reporter{..} = do
       STM.atomically $ aha board reporterKey
       receive
 
+
+
+
+
+data ReporterJO = ReporterJO
+  { jReporterKey  :: ReporterKey
+  , jReporterAha  :: Int
+  }
+
+data BoardJO = BoardJO
+  { jBoardSecretKey :: BoardSecretKey
+  , jBoardPublicKey :: BoardPublicKey
+  , jBoardCaption   :: BoardCaption
+  , jBoardAha       :: Int
+  , jBoardReporters :: Map.Map ReporterKey ReporterJO
+  }
+
+data ServerJO = ServerJO
+  { jServerBoards    :: Map.Map BoardPublicKey BoardJO
+  , jServerBoardKeys :: Map.Map BoardSecretKey BoardPublicKey
+  }
+
+
+reporterToJO :: Reporter -> STM.STM ReporterJO
+reporterToJO Reporter{..} = do
+  aha <- STM.readTVar reporterAha
+  return ReporterJO { jReporterKey = reporterKey
+                    , jReporterAha = aha
+                    }
+
+reporterFromJO :: ReporterJO -> STM.STM Reporter
+reporterFromJO ReporterJO{..} = do
+  aha   <- STM.newTVar jReporterAha
+  chan  <- STM.newBroadcastTChan 
+  return Reporter { reporterKey  = jReporterKey
+                  , reporterAha  = aha
+                  , reporterChan = chan
+                  }
+  
+
+
+
+boardToJO :: Board -> STM.STM BoardJO
+boardToJO Board{..} = do
+  aha <- STM.readTVar boardAha
+  reporters <- STM.readTVar boardReporters
+  reporters' <- reportersToJOs reporters
+  return BoardJO { jBoardSecretKey = boardSecretKey
+                 , jBoardPublicKey = boardPublicKey
+                 , jBoardCaption   = boardCaption
+                 , jBoardAha       = aha
+                 , jBoardReporters = reporters'
+                 }
+    where
+      reporterToJO' :: (ReporterKey,Reporter) -> STM.STM (ReporterKey,ReporterJO)
+      reporterToJO' (k,i) = do
+        i' <- reporterToJO i
+        return (k,i')
+
+      reportersToJOs :: Map.Map ReporterKey Reporter -> STM.STM (Map.Map ReporterKey ReporterJO)
+      reportersToJOs reporters = do
+        reporters' <- mapM reporterToJO' $ Map.toList reporters
+        return $ Map.fromList reporters'
+      
+
+boardFromJO :: BoardJO -> STM.STM Board
+boardFromJO BoardJO{..} = do
+  aha        <- STM.newTVar jBoardAha
+  reporters  <- reportersFromJOs jBoardReporters
+  reporters' <- STM.newTVar reporters
+  chan       <- STM.newBroadcastTChan 
+  return Board { boardSecretKey = jBoardSecretKey
+               , boardPublicKey = jBoardPublicKey
+               , boardCaption   = jBoardCaption
+               , boardAha       = aha
+               , boardReporters = reporters'
+               , boardChan      = chan
+               }
+
+    where
+      reporterFromJO' :: (ReporterKey,ReporterJO) -> STM.STM (ReporterKey,Reporter)
+      reporterFromJO' (k,i) = do
+        i' <- reporterFromJO i
+        return (k,i')
+
+      reportersFromJOs :: Map.Map ReporterKey ReporterJO -> STM.STM (Map.Map ReporterKey Reporter)
+      reportersFromJOs reporters = do
+        reporters' <- mapM reporterFromJO' $ Map.toList reporters
+        return $ Map.fromList reporters'
+
+
+
+serverToJO :: Server -> IO ServerJO
+serverToJO Server{..} = do
+
+  -- SecretKeyとPublicKeyが矛盾しないように
+  (keys,boards) <- STM.atomically $
+                   (,) <$> STM.readTVar serverBoardKeys <*> STM.readTVar serverBoards
+
+  boards' <- boardsToJOs boards
+  return ServerJO { jServerBoards    = boards'
+                  , jServerBoardKeys = keys
+                  }
+    where
+      boardToJO' :: (BoardPublicKey,Board) -> IO (BoardPublicKey,BoardJO)
+      boardToJO' (k,i) = do
+        i' <- STM.atomically $ boardToJO i -- BoardとReporterが矛盾しないように
+        return (k,i')
+
+      boardsToJOs :: Map.Map BoardPublicKey Board -> IO (Map.Map BoardPublicKey BoardJO)
+      boardsToJOs boards = do
+        boards' <- mapM boardToJO' $ Map.toList boards
+        return $ Map.fromList boards'
+
+
+serverFromJO :: ServerJO -> STM.STM Server
+serverFromJO ServerJO{..} = do
+  boards  <- boardsFromJOs jServerBoards
+  boards' <- STM.newTVar boards
+  keys    <- STM.newTVar jServerBoardKeys
+  return Server { serverBoards = boards'
+                , serverBoardKeys = keys
+                }
+    where
+      boardFromJO' :: (BoardPublicKey,BoardJO) -> STM.STM (BoardPublicKey,Board)
+      boardFromJO' (k,i) = do
+        i' <- boardFromJO i
+        return (k,i')
+
+      boardsFromJOs :: Map.Map BoardPublicKey BoardJO -> STM.STM (Map.Map BoardPublicKey Board)
+      boardsFromJOs boards = do
+        boards' <- mapM boardFromJO' $ Map.toList boards
+        return $ Map.fromList boards'
